f9437c03f81ea3aa06d59692c197cebfda418475
Razvan Rusu <razvanrusu9420@gmail.com>
2025-08-08 18:06:23 +0300

    prepare credentials provider

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,5 +2,5 @@ import { NextRequest } from "next/server";
 
 export function POST(request: NextRequest) {
   const body = request.body;
-  console.log(body);
+  console.log("Console form post route");
 }
--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/app/libs/prisma";
@@ -11,12 +12,39 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google,
     Credentials({
       credentials: {
-        username: { label: "Name" },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        console.log(credentials);
-        throw new Error();
+        if (
+          !credentials?.email ||
+          typeof credentials?.email !== "string" ||
+          !credentials.password ||
+          typeof credentials.password !== "string"
+        ) {
+          throw new Error("Invalid credentials");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return user;
       },
     }),
   ],
